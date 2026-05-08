8f175ef7dc4de19e903f1615ebdbcaa9d58f01ef
nathan van lomwel <nvanlomwel@tudelft.nl>
2026-05-08 09:30:26 +0000

    wrapper

---
+++ COFLEX/COFLEXOpt/functions/nlp_solver_dspace_wrapper.h
@@ -0,0 +1,7 @@
+// nlp_solver_dspace_wrapper.h
+#ifndef NLP_SOLVER_DSPACE_WRAPPER_H
+#define NLP_SOLVER_DSPACE_WRAPPER_H
+
+int solve_nlp_realtime(double wind_speed, double* rotor_speed, double* pitch_angle);
+
+#endif
